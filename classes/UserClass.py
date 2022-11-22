4625ada6c19cd95920ed8e463209f1f6935a2e59
wekwas <90058284+wekwas@users.noreply.github.com>
2022-11-22 12:23:51 -0600

    Classes for testing

---
+++ classes/UserClass.py
@@ -0,0 +1,45 @@
+from SchedulingAPP.models import
+
+class UserClass:
+    def __init__(self, username, password, email, first_name, last_name):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def set_username(self, new_username):
+        self.username = new_username
+
+    def get_username(self):
+        return self.username
+
+    def set_password(self, new_username):
+        self.username = new_username
+
+    def get_password(self):
+        return self.username
+
+    def set_username(self, new_username):
+        self.username = new_username
+
+    def get_username(self):
+        return self.username
+
+    def set_email(self, new_username):
+        self.username = new_username
+
+    def get_email(self):
+        return self.username
+
+    def set_first_name(self, new_username):
+        self.username = new_username
+
+    def get_first_name(self):
+        return self.username
+
+    def set_last_name(self, new_username):
+        self.username = new_username
+
+    def get_last_name(self):
+        return self.username
