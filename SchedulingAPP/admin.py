dd5c24ccefd7b7f2e956cd063a6df555ba5501b3
collinsmac <canglese@uwm.edu>
2022-12-06 15:53:33 -0600

    added notification model

--- SchedulingAPP/admin.py
+++ SchedulingAPP/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import User, Course, Section
+from .models import User, Course, Section, Notification
 
 admin.site.register(User)
 admin.site.register(Course)
 admin.site.register(Section)
+admin.site.register(Notification)
